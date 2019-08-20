adf3940809be6c6ef64cbe41537c89c91328e34a
Hyo Gyeong <cdkrcd8@gmail.com>
2019-08-20 23:38:40 +0900

    자바스크립트 복습

---
+++ 3.personalinfo.js
@@ -0,0 +1,4 @@
+document.write("<div style='font-weight: bold; color: purple; font-size:30px;'>개인 정보</div>");
+
+var age=23, name = '신효경', school = '동덕여자대학교';
+document.write("나이 : " + age +"<br> 이름 : " + name + "<br> 학교 : " + school);
---
+++ 외부 자바 파일/3.personalinfo2.js
@@ -0,0 +1,6 @@
+document.write("<div style='font-weight: bold; color: pink; font-size:30px;'>개인 정보</div>");
+
+var age=20;
+var name = '신혜윤';
+var school = '메가스터디';
+document.write("나이 : " + age +"<br> 이름 : " + name + "<br> 학교 : " + school);
